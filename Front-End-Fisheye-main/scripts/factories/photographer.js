2e8137a12b33e5cb02a9af361cbb33e3faed62e5
Fabrice BB2022 <forza_91@msn.com>
2022-07-28 12:30:46 +0200

    add modification

--- Front-End-Fisheye-main/scripts/factories/photographer.js
+++ Front-End-Fisheye-main/scripts/factories/photographer.js
@@ -1,17 +1,46 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { id, name, portrait, city, country, tagline, tags, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        img.setAttribute("src", picture )
+
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+
+        const h3 = document.createElement("h3");
+        h3.textContent = `${city}, ${country}`;
+
+        const h4 = document.createElement("h4");
+        h4.textContent = tagline;
+
+        const span = document.createElement("span")
+        span.textContent = tags;
+
+        const p = document.createElement("p");
+        p.textContent = `${price}€/jour`;
+
+        const ahref = document.createElement("a");
+        ahref.setAttribute("class", "link");
+        ahref.setAttribute("aria-label", `${name}`);
+        ahref.setAttribute("href", `photographer.html?id=${id}`);
+        
+        ahref.appendChild(article);
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(h3);
+        article.appendChild(h4);
+        article.appendChild(p);
+        article.appendChild(tagline);
+        article.appendChild(tags);
+        article.appendChild(price);
+
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { id, name, picture, city, country, tagline, tags, price, getUserCardDOM }
+ 
 }
