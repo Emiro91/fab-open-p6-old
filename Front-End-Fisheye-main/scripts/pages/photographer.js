bd7d32e5fa63e9fc593c5e2bf4951bbbbaacc2cb
Fabrice BB2022 <forza_91@msn.com>
2022-08-04 09:40:33 +0200

    test recuperation id

--- Front-End-Fisheye-main/scripts/pages/photographer.js
+++ Front-End-Fisheye-main/scripts/pages/photographer.js
@@ -1,2 +1,13 @@
 //Mettre le code JavaScript lié à la page photographer.html
 
+window.addEventListener('load',() => {
+let searchParams = new URLSearchParams(window.location.search);
+
+if (searchParams.has('id')) {
+    let photographerId = searchParams.get('id')
+} else {
+    window.location.pathname = 'Front-End-Fisheye-main'
+    
+}
+})
+return false;
