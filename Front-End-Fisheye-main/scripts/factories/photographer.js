3ee65cd65535cfb7ac41cd24ead6901d55151171
Fabrice BB2022 <forza_91@msn.com>
2022-08-12 20:08:16 +0200

    modifications des fichiers html et css

--- Front-End-Fisheye-main/scripts/factories/photographer.js
+++ Front-End-Fisheye-main/scripts/factories/photographer.js
@@ -1,16 +1,24 @@
 function photographerFactory(data) {
-    const { id, name, portrait, city, country, tagline,price } = data;
+    const {
+        id,
+        name,
+        portrait,
+        city,
+        country,
+        tagline,
+        price
+    } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
+        const article = document.createElement('article');
 
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture )
-        img.setAttribute("class","card-img")
+        const img = document.createElement('img');
+        img.setAttribute("src", picture)
+        img.setAttribute("class", "card-img")
 
-        const h2 = document.createElement( 'h2' );
+        const h2 = document.createElement('h2');
         h2.textContent = name;
 
         const h3 = document.createElement("h3");
@@ -26,7 +34,7 @@ function photographerFactory(data) {
         ahref.setAttribute("class", "link");
         ahref.setAttribute("aria-label", `${name}`);
         ahref.setAttribute("href", `photographer.html?id=${id}`);
-        
+
         ahref.appendChild(article);
         article.appendChild(img);
         article.appendChild(h2);
@@ -36,6 +44,73 @@ function photographerFactory(data) {
 
         return ahref;
     }
-    return { id, name, picture, city, country, tagline, price, getUserCardDOM }
- 
+
+    function getProfilePage() {
+        const photographHeader = document.createElement("div");
+        photographHeader.setAttribute("class", "photograph-header");
+
+        const mainInfos = document.createElement("div");
+        mainInfos.setAttribute("class", "photograph-infos");
+        mainInfos.setAttribute("tabindex", "0");
+
+        const fullname = document.createElement("h1");
+        fullname.setAttribute("class", "names");
+        fullname.textContent = name;
+
+        const location = document.createElement("div");
+        location.setAttribute("class", "location");
+        const villePays = document.createElement("span");
+        villePays.textContent = city.concat(", ", country);
+        const bio = document.createElement("p");
+        bio.textContent = tagline;
+
+        const contactButton = document.createElement("button");
+        contactButton.setAttribute("id", "contact_button");
+        contactButton.setAttribute("class", "contact_button");
+        contactButton.setAttribute("onclick", "displayModal()");
+        contactButton.textContent = "Contactez-moi";
+
+        const profileImg = document.createElement("img");
+        profileImg.setAttribute("src", `assets/photographers/${portrait}`);
+        profileImg.setAttribute("class", "photographImg");
+        profileImg.setAttribute("alt", name);
+        profileImg.setAttribute("tabindex", "0");
+
+        const priceLikes = document.createElement("div");
+        priceLikes.setAttribute("class", "price");
+        const likesSpan = document.createElement("span");
+        likesSpan.setAttribute("class", "compteurLikeTotal");
+
+        const heart = document.createElement("span");
+        heart.setAttribute("aria-label", "likes");
+        heart.setAttribute("class", "fas fa-heart");
+        const priceDay = document.createElement("span");
+        priceDay.textContent = `${price}€ /jour`;
+
+        photographHeader.appendChild(mainInfos);
+        mainInfos.appendChild(fullname);
+        mainInfos.appendChild(location);
+        location.appendChild(villePays);
+        location.appendChild(bio);
+        photographHeader.appendChild(contactButton);
+        photographHeader.appendChild(profileImg);
+
+        photographHeader.appendChild(priceLikes);
+        priceLikes.appendChild(likesSpan);
+        likesSpan.appendChild(heart);
+        priceLikes.appendChild(priceDay);
+        return photographHeader;
+    }
+    return {
+        id,
+        name,
+        picture,
+        city,
+        country,
+        tagline,
+        price,
+        getUserCardDOM,
+        getProfilePage,
+    }
+
 }
--- Front-End-Fisheye-main/scripts/pages/photographer.js
+++ Front-End-Fisheye-main/scripts/pages/photographer.js
@@ -1,15 +1,35 @@
-//Mettre le code JavaScript lié à la page photographer.html
+async function getPhotographer() {
+    const url = new URL(window.location.href);
+    const id = url.searchParams.get("id");
 
-/*window.addEventListener('load',() => {
-let searchParams = new URLSearchParams(window.location.search);
+    // Penser à remplacer par les données récupérées dans le json
+    const photographerData = await (
+        fetch('./data/photographers.json')
+        .then((response) => response.json())
+    );
+    photographerData.photographers = photographerData.photographers.filter(
+        (photographer) => photographer.id == id
+    )[0];
+    photographerData.media = photographerData.media.filter(
+        (media) => media.photographerId == id
+    );
+    return photographerData;
 
-if (searchParams.has('id')) {
-    let photographerId = searchParams.get('id')
-    console.log();
-} else {
-    window.location.pathname = ('index.html');   
-    console.log();
+    // et bien retourner le tableau photographers seulement une fois
+
 }
-},
-false);*/
 
+async function displayData(photographer) {
+    const photographersSection = document.getElementById("photographeProfile");
+    const profileModel = photographerFactory(photographer);
+    const profiles = profileModel.getProfilePage();
+    photographersSection.appendChild(profiles);
+};
+
+async function init() {
+    // Récupère les datas des photographes
+    const photographer = await getPhotographer();
+    displayData(photographer.photographers);
+};
+
+init();
